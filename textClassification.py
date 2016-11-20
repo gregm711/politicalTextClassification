2fbb655039e177c1d6d482b6d873df03b299e228
gregm711 <miller.7285osu@gmail.com>
2016-11-20 01:09:16 -0500

    ready for initial testing

--- textClassification.py
+++ textClassification.py
@@ -16,9 +16,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk.classify.util
 from nltk.stem.porter import *
 
-
-
-
 """
 RESULTS:
 Count Vectorizer:
@@ -53,6 +50,14 @@ Stemmed And Tokenized:
 			0.909583069676
 		0.93004288828
 
+abortion = folder + 'abortion'
+ 	creation = folder  + 'creation'
+ 	gayRights = folder + 'gayRights'
+ 	god = folder +  'god'
+ 	guns = folder  + 'guns'
+ 	healthcare = folder + 'healthcare'
+
+
 """
 def tokenize(text):
 	return [token for token in simple_preprocess(text) if token not in STOPWORDS]
@@ -65,8 +70,6 @@ def stem(tokens):
 
 
 def testClassifier(testObjects, testTargets, classifier, vectorizer):
-
-
 	exampleCounts = vectorizer.transform(testObjects)
 	predictions = classifier.predict(exampleCounts)
 	print np.mean(predictions == testTargets)
@@ -75,60 +78,6 @@ def testClassifier(testObjects, testTargets, classifier, vectorizer):
 
 def createSentence(textAsList):
 	return' '.join(map(str, textAsList))
-
-# def createClassifier():
-# 	text = []
-# 	targets = []	
-# 	count = 0
-# 	together  = []
-# 	t1 = time.time()
-# 	with open('formattedDebate.csv', 'r') as csvFile:
-# 		reader = csv.reader(csvFile)
-# 		for row in reader:
-# 			count  += 1
-# 			try:
-# 				encoded = row[1].decode('utf-8','ignore').encode('utf-8', 'ignore')
-# 				sentences = encoded.split('.')
-# 				for sentence in sentences:
-# 					tokens = tokenize(sentence)
-# 					stems = stem(tokens)
-# 					newSent = createSentence(tokens)
-# 					together.append((newSent, int(row[0])))
-# 			except Exception as e :
-# 				print e 
-# 		print 'done' , time.time() - t1 , count
-
-# 		random.shuffle(together)
-		# trainObjects  =   [sentence for (sentence , val) in together[:int(len(together) * 0.8)]]
-		# trainTargets  =   [val for (sent, val) in together[:int(len(together) * 0.8)]]
-		# # trainObjects  =   [sentence for (sentence , val) in together]
-		# # trainTargets  =   [val for (sent, val) in together]
-		# testObjects   =   [sentence for (sentence , val) in together[:int(len(together) * .2)]]
-		# testTargets   =   [val for (sentence , val) in together[:int(len(together) * .2)]]
-			
-# 		# vectorizer = TfidfVectorizer()
-# 		vectorizer = CountVectorizer()
-# 		counts = vectorizer.fit_transform(trainObjects)
-# 		classifier = MultinomialNB(alpha = 0.5)
-# 		classifier.fit(counts, trainTargets)
-
-# 		testClassifier(testObjects, testTargets, classifier, vectorizer)
-
-# 		classifierFile = open('topicsClassifierStemmedTokenized.pkl', 'w')
-# 		vectorizerFile = open('topicsVectorizerStemmedTokenized.pkl', 'w')
-# 		pickle.dump(classifier, classifierFile)
-# 		pickle.dump(vectorizer, vectorizerFile)
-# 		print 'done'
-
-
-"""
-abortion = folder + 'abortion'
- 	creation = folder  + 'creation'
- 	gayRights = folder + 'gayRights'
- 	god = folder +  'god'
- 	guns = folder  + 'guns'
- 	healthcare = folder + 'healthcare'
-"""
 
 def createIndividuleClassifier(topic, sentencesBool):
 	text = []
@@ -159,28 +108,17 @@ def createIndividuleClassifier(topic, sentencesBool):
 		random.shuffle(together)
 		trainObjects  =   [sentence for (sentence , val) in together[:int(len(together) * 0.8)]]
 		trainTargets  =   [val for (sent, val) in together[:int(len(together) * 0.8)]]
-		# trainObjects  =   [sentence for (sentence , val) in together]
-		# trainTargets  =   [val for (sent, val) in together]
 		testObjects   =   [sentence for (sentence , val) in together[:int(len(together) * .2)]]
 		testTargets   =   [val for (sentence , val) in together[:int(len(together) * .2)]]
-			
-		# vectorizer = TfidfVectorizer()
-		print testTargets.count(1)
-		print testTargets.count(2)
-		print trainTargets.count(1)
-		print trainTargets.count(2)
 		vectorizer = TfidfVectorizer()
 		counts = vectorizer.fit_transform(trainObjects)
 		classifier = MultinomialNB(alpha = 0.5)
 		classifier.fit(counts, trainTargets)
-
 		testClassifier(testObjects, testTargets, classifier, vectorizer)
-
 		classifierFile = open('topicsClassifierStemmedTokenized' + topic  + sentencesBool + '.pkl', 'w')
 		vectorizerFile = open('topicsVectorizerStemmedTokenized' + topic + sentencesBool + '.pkl', 'w')
 		pickle.dump(classifier, classifierFile)
 		pickle.dump(vectorizer, vectorizerFile)
-		print 'done'
 
 
 # createIndividuleClassifier('Debate', '')
@@ -196,44 +134,45 @@ def processArray(textArray):
 	return result
 
 
-def classifyText(text, topic, sentencesBool):
-	classifierFile = open('topicsClassifierStemmedTokenized' + topic + sentencesBool +  '.pkl', 'rb')
-	vectorizerFile = open('topicsVectorizerStemmedTokenized' + topic + sentencesBool +'.pkl', 'rb')
-	if sentencesBool == 'sentences':
+def topicClassifier(text, topic, sentencesTag):
+	classifierFile = open('topicsClassifierStemmedTokenized' + topic + sentencesTag +  '.pkl', 'rb')
+	vectorizerFile = open('topicsVectorizerStemmedTokenized' + topic + sentencesTag +'.pkl', 'rb')
+	if sentencesTag == 'sentences':
 		sentences = text[0].split('.')
 		finalText  = processArray(sentences)
-	elif sentencesBool != 'sentences':
+	elif sentencesTag != 'sentences':
 		finalText = processArray(text)
 	classifier = pickle.load(classifierFile)
 	vectorizer = pickle.load(vectorizerFile)
 	finalCounts = vectorizer.transform(finalText)
 	confidence = classifier.predict_proba(finalCounts)
 	prediction = classifier.predict(finalCounts)
+	return prediction , confidence , finalText
 
-	return prediction , confidence
+
+def classifyTopic(examples, topic):
+	confidenceThreshold = 0.6
+	usingSentences , sentencesConfidence , finalText = topicClassifier(examples, topic, 'sentences')
+	results = []
+	for x in  xrange(len(sentencesConfidence) - 1):
+		maxVal = max(sentencesConfidence[x][0], sentencesConfidence[x][1])
+		if maxVal >= confidenceThreshold:
+			confidence  = sentencesConfidence[x]
+			tmpTup = (usingSentences[x], finalText[x], maxVal)
+			results.append(tmpTup)
+	noSentences    , noSentencesConfidence, finalText = topicClassifier(examples, topic, '')
+	maxVal = max(noSentencesConfidence[0][0], noSentencesConfidence[0][1])
+	if maxVal >= confidenceThreshold:
+		tmpTup = (noSentences[0], finalText[0], maxVal)
+		results.append(tmpTup)
+
+	return results
+
+
 
 examples = ['Science says it\'s not a baby.I beg to differ.Science said it is a baby.The baby has its own DNA and at nine weeks he has a heartbeat.A baby at twelve weeks is very']
 
-
-print classifyText(examples, 'abortion', '')
-
-# def testClassifier():
-
-
-# 	classifierFile = open('topicsClassifierStemmedTokenized.pkl', 'rb')
-# 	vectorizerFile = open('topicsVectorizerStemmedTokenized.pkl', 'rb')
-# 	vectorizer = pickle.load(vectorizerFile)
-# 	classifier = pickle.load(classifierFile)
-# 	examples = ['Science says it\'s not a baby.I beg to differ.Science said it is a baby.The baby has its own DNA and at nine weeks he has a heartbeat.A baby at twelve weeks is very']
-	
-# 	example_counts = vectorizer.transform(examples)
-# 	predictions = classifier.predict(example_counts)
-# 	predictions
-# 	print predictions
-
-# # testClassifier()
-
-# print classifyText(examples)
+classifyTopic(examples, 'abortion')
 
 
 
